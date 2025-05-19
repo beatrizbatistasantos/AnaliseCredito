3bcf21e606d37805b88546779c43f9ad02665044
Beatriz Batista <byah24@outlook.com>
2025-05-18 23:51:05 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿decimal rendaMensal, valorEmprestimo;
+int qtdParcelas;
+
+Console.WriteLine("===== Bem-vindo à Análise de Crédito =====");
+
+Console.Write("Digite a sua Renda Mensal em R$:");
+while (!decimal.TryParse(Console.ReadLine(), out rendaMensal) || rendaMensal <= 0)
+{
+    Console.WriteLine("Valor inválido! Digite um valor numérico positivo para a renda mensal.");
+}
+
+Console.Write("Digite o Valor que deseja pegar emprestado em R$:");
+while (!decimal.TryParse(Console.ReadLine(), out valorEmprestimo) || valorEmprestimo <= 0)
+{
+    Console.WriteLine("Valor inválido! Digite um valor numérico positivo para o valor do empréstimo.");
+}
+
+Console.Write("Digite em quantas parcelas deseja pagar:");
+while (!int.TryParse(Console.ReadLine(), out qtdParcelas) || qtdParcelas <= 0)
+{
+    Console.WriteLine("Valor inválido! Digite um valor inteiro positivo para a quantidade de parcelas.");
+}
+
+decimal valorParcela = valorEmprestimo / qtdParcelas;
+decimal limiteEmprestimo = rendaMensal * 0.3m;
+
+if (valorParcela <= limiteEmprestimo)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine("Parabéns! Empréstimo autorizado.");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("Empréstimo negado, aumente sua renda ou solicite um valor menor!");
+    Console.ResetColor();
+}
+
+Console.WriteLine();
+Console.WriteLine("Aperte qualquer tecla para finalizar a aplicação...");
+Console.ReadKey();
+Console.Clear();
